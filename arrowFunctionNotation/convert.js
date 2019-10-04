17040581df364d0ef03b2ea42654c14a261096c3
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2019-10-04 09:16:47 +0200

    practice

---
+++ arrowFunctionNotation/convert.js
@@ -0,0 +1,25 @@
+// function addToTen(num) {
+//   return 10 + num;
+// }
+
+const addToTen =  num => 10 + num;
+
+console.log(addToTen(10));
+
+// function divideUs(num1, num2) {
+//   return num1/num2;
+// }
+const divideUs =  (num1, num2) => num1/num2;
+console.log(divideUs(1,2));
+
+// function printMyName() {
+//   let myName = 'Ashley';
+// 	console.log(myName);
+// }
+
+const printMyName =  () => {
+  let myName = 'Ashley';
+	console.log(myName);
+}
+
+printMyName();
---
+++ arrowFunctionNotation/refactor.js
@@ -0,0 +1,33 @@
+// const multiplyByTen = (num) => {
+// 	return 10 * num;
+// }
+
+const multiplyByTen = num =>  10 * num;
+console.log(multiplyByTen(2));
+
+
+// const printToTen = () => {
+// 	for (let i = 1; i <= 10; i++) {
+// 		console.log(i);
+// 	}
+// }
+
+const printToTen = () => {
+	for (let i = 1; i <= 10; i++) {
+		console.log(i);
+	}
+};
+
+console.log(printToTen());
+
+
+
+// const addUs = (num1, num2) => {
+// 	console.log(num1 + num2);
+// }
+
+const addUs = (num1, num2) => {
+	console.log(num1 + num2);
+}
+
+console.log(addUs(4,4));
